dcf71c45eee3c5ca03c3ca43e1b549a247396045
PopaJopa12 <tmwsls12@gmail.com>
2019-05-25 22:04:22 +0300

    third commit

---
+++ scriptegg.js
@@ -0,0 +1,8 @@
+$(function(){
+	$("#easteregg").click(function(){
+		$(".logo img").attr("src","pic/eastereggback.jpg")
+	})
+	$("#easteregg").dblclick(function(){
+		$(".logo img").attr("src","pic/logo.jpg")
+	})
+})
--- scripts.js
+++ scripts.js
@@ -45,3 +45,11 @@ $(function () {
 		change ();
 	});	
 });
+$(function(){
+	$("#easteregg").click(function(){
+		$(".logo img").attr("src","pic/eastereggback.jpg")
+	})
+	$("#easteregg").dblclick(function(){
+		$(".logo img").attr("src","pic/logo.jpg")
+	})
+})
